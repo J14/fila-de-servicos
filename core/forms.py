d738cc465e2bbce44ecc7cd296310b8bdbce9107
João Victor <joaoaraujo.367@gmail.com>
2020-04-21 12:22:44 -0300

    Change the form of Queue to validate attended and attending fields

---
+++ core/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+
+from .models import Queue
+
+
+class QueueForm(forms.ModelForm):
+    class Meta:
+        model = Queue
+        fields = ['person', 'service', 'attending', 'attended']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        attending = cleaned_data.get("attending")
+        attended = cleaned_data.get("attended")
+
+        if attending and attended:
+            raise forms.ValidationError(
+                "There can not be attending"
+                " and attended at the same time"
+            )
